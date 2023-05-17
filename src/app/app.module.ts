480b5189fd5722bd283684183734f8f55cdd43ee
Elvan Hidayat <elvanhd@gmail.com>
2023-05-17 19:02:25 +0700

    dynamic search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductCategoryMenuComponent,
+    ProductListComponent,
+  ],
   imports: [AppRoutingModule, BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
