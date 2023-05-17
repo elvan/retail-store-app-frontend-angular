4549da7de04f42ee3780952defec9f26dd1cecbc
Elvan Hidayat <elvanhd@gmail.com>
2023-05-17 21:01:55 +0700

    search for products by keyword

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductCategoryMenuComponent,
     ProductListComponent,
+    SearchComponent,
   ],
   imports: [AppRoutingModule, BrowserModule, HttpClientModule],
   providers: [],
