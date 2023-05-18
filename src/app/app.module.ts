4b307099d7b2982fcacbb0f2627fe231c88f53a5
Elvan Hidayat <elvanhd@gmail.com>
2023-05-18 21:36:18 +0700

    checkout-form layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -16,12 +18,19 @@ import { SearchComponent } from './components/search/search.component';
     AppComponent,
     CartDetailsComponent,
     CartStatusComponent,
+    CheckoutComponent,
     ProductCategoryMenuComponent,
     ProductDetailsComponent,
     ProductListComponent,
     SearchComponent,
   ],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, NgbModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
