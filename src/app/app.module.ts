1860b3b1c585b7436d8e68270627d44eefb9943c
Elvan Hidayat <elvanhd@gmail.com>
2023-05-18 14:51:42 +0700

    create the cart-status component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -12,6 +13,7 @@ import { SearchComponent } from './components/search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
+    CartStatusComponent,
     ProductCategoryMenuComponent,
     ProductDetailsComponent,
     ProductListComponent,
